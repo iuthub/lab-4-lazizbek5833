71c400e3a670f3c3538c7927bd27f426b65c66c3
lazizbek5833 <48064492+lazizbek5833@users.noreply.github.com>
2019-03-10 10:16:40 +0500

    Add files via upload

---
+++ webpage/music.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Music Viewer</title>
+    <link rel="stylesheet" href="viewer.css" type="text/css">
+</head>
+<body>
+    <div id="header">
+
+			<h1>190M Music Playlist Viewer</h1>
+			<h2>Search Through Your Playlists and Music</h2>
+	</div>
+
+</body>
+</html>
